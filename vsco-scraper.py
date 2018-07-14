6a442419d491403e150675dc856b7ff3bc00dd41
Omar Quazi <omar@quazi.co>
2018-07-14 16:49:35 -0400

    Create vsco-scraper.py

---
+++ vsco-scraper.py
@@ -0,0 +1,69 @@
+import requests, json, urllib, os, errno
+
+
+def scrape_vsco(username):
+
+    # List of photos
+    pictures = []
+
+    s = requests.session()
+
+    page_number = 1
+
+    # Create the directory:
+    directory = username
+    try:
+        os.makedirs(directory)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            directory = directory+"-"+str(random.randint(0, 5000))
+            os.makedirs(directory)
+    
+    os.chdir(directory)
+
+    # Grab the profile
+    profile = s.get("https://vsco.co/"+username+"/images/"+str(page_number))
+
+    # Data from html page
+    contents = profile.text
+
+    # Profile picture
+    profile_picture_link = contents.split('"siteProfileImageUrl":"')[1]
+    profile_picture_link = profile_picture_link[:profile_picture_link.index('"')]
+
+    # Save profile picture
+    urllib.urlretrieve(profile_picture_link, "profile_picture.jpg")
+    print "Downloading profile picture..."
+
+
+    while True:
+
+        # Grab the full data for this page
+        data = contents.split("window.__PRELOADED_STATE__ =")[1]
+        data = data[:data.index("</script>")]
+
+        # Convert string to JSON format
+        data = json.loads(data)
+
+        if len(data["entities"]["images"]) == 0:
+            print "Done."
+            break
+
+        print "Downloading page " + str(page_number) + " ..."
+
+        # Grab the images
+        for x in data["entities"]["images"]:
+            if data["entities"]["images"][x]["isVideo"]:
+                urllib.urlretrieve("http://"+data["entities"]["images"][x]["videoUrl"], x+".mp4")
+            else:
+                urllib.urlretrieve("http://"+data["entities"]["images"][x]["responsiveUrl"], x+".jpg")
+            pictures.append(x)
+
+        page_number += 1
+        # Grab the profile
+        profile = s.get("https://vsco.co/"+username+"/images/"+str(page_number))
+
+        # Data from html page
+        contents = profile.text
+
+    
